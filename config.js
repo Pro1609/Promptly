7ce95eac42aaf0329bafa71b104a1940c43e3c8c
Shadab Raza <98049489+Pro1609@users.noreply.github.com>
2025-07-17 09:26:32 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Configuration injection for Firebase
-// Generated from environment variables
-window.FIREBASE_CONFIG = {
-    apiKey: "AIzaSyCYAedjYF8ro1vseBCRDYKmD4pRUpslGjE",
-    authDomain: "aigpt-d6410.firebaseapp.com",
-    projectId: "aigpt-d6410",
-    storageBucket: "aigpt-d6410.firebasestorage.app",
-    appId: "1:707699404468:web:71f16cfefcb20a0dacf1e1"
-};
